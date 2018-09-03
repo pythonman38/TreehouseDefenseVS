be3c7c69a907d72eb50d348fb261a0c6ba4b4ed7
Michael Walker <pythonk@email.phoenix.edu>
2018-09-03 19:17:31 -0400

    further enhancements to defense game

--- TreehouseDefense/Tower.cs
+++ TreehouseDefense/Tower.cs
@@ -5,9 +5,9 @@ namespace TreehouseDefense
 {
     class Tower
     {
-        private const int _range = 1;
-        private const int _power = 1;
-        private const double _accuracy = .75;
+        protected virtual int Range { get; } = 1;
+        protected virtual int Power { get; } = 1;
+        protected virtual double Accuracy { get; } = .75;
 
         private static readonly Random _random = new Random();
 
@@ -20,18 +20,18 @@ namespace TreehouseDefense
 
         public bool IsSuccessfulShot()
         {
-            return _random.NextDouble() < _accuracy;
+            return _random.NextDouble() < Accuracy;
         }
 
         public void FireOnInvaders(Invader[] invaders)
         {
             foreach(Invader invader in invaders)
             {
-                if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
+                if(invader.IsActive && _location.InRangeOf(invader.Location, Range))
                 {
                     if (IsSuccessfulShot())
                     {
-                        invader.DecreaseHealth(_power);
+                        invader.DecreaseHealth(Power);
                         
                         if (invader.IsNetrualized)
                         {
